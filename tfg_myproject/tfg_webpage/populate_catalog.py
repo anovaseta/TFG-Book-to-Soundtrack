e6c3f414c646985969b5f0f3c3184a0c8efde391
manuloseta <manuloseta@gmail.com>
2026-01-11 20:22:14 +0100

    db updated, now fully populated

--- tfg_myproject/tfg_webpage/populate_catalog.py
+++ tfg_myproject/tfg_webpage/populate_catalog.py
@@ -10,6 +10,7 @@
 import os
 import django
 import json
+import pylast
 from django.utils.html import strip_tags
 from PyMultiDictionary import MultiDictionary
 
@@ -18,7 +19,7 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 
-from webapp.models import Storygraph_Book, Storygraph_Tag, Tagged_Book, Synonym, Synonym_Relation
+from webapp.models import Storygraph_Book, Storygraph_Tag, Tagged_Book, Synonym, Synonym_Relation, LastFM_Entity, Entity_Tag_Relation
 
 django.setup()
 
@@ -90,18 +91,24 @@ def populate_synonyms_from_tags():
     # initalize PyMultiDictionary
     dict = MultiDictionary()
 
+    syn_total_list = []
+
     # save synonyms only for MOOD-type tags for now
     for obj_tag in all_tags:
 
         tag = obj_tag.tag_name
         syn_list = dict.synonym('en', tag)
         syn_list = syn_list + [tag] # add tag itself 'as a synonym'
+        print("processing", tag, "with", len(syn_list), "synonyms")
 
         # print(tag, syn_list)
         # print('----------------------------------------------------------------')
 
         # create synonym instances, and their relationship to the tag
         for syn in syn_list:
+            if syn in syn_total_list:
+                continue
+
             if syn == tag:
                 type = 'SELF' # add tag itself 'as a synonym'
             else:
@@ -119,14 +126,71 @@ def populate_synonyms_from_tags():
                 synonym=obj_syn
             )
 
+            syn_total_list += [syn]
+        
+    print(len(syn_total_list))
 
 
+def populate_lastfm_from_synonyms():
 
-    
+    in_path = os.path.join("../", "project_misc", "complete_tag_search.json")
+    in_file = open(in_path, "r")
+    in_dict = json.load(in_file)
+
+    # get synonyms from db
+    all_synonyms = [syn.synonym for syn in Synonym.objects.all()]
+
+    total = len(in_dict.keys())
+    counter = 1
+
+    for key,value in in_dict.items():
+
+        syn = Synonym.objects.get(synonym=key)
+
+        print("Completing tag", key)
+        print("Tag", counter, "in", total)
+
+        for artist in value["top_artists"]:
+            obj, created = LastFM_Entity.objects.get_or_create(
+                name=artist,
+                type="ARTIST"
+            )
+
+            Entity_Tag_Relation.objects.get_or_create(
+                entity=obj,
+                tag=syn
+            )
+
+        for album in value["top_albums"]:
+            obj, created = LastFM_Entity.objects.get_or_create(
+                name=album,
+                type="ALBUM"
+            )
+
+            Entity_Tag_Relation.objects.get_or_create(
+                entity=obj,
+                tag=syn
+            )
+
+        for track in value["top_tracks"]:
+            obj, created = LastFM_Entity.objects.get_or_create(
+                name=track,
+                type="TRACK"
+            )
+
+            Entity_Tag_Relation.objects.get_or_create(
+                entity=obj,
+                tag=syn
+            )
+
+        counter += 1
+
+        
 
 def populate():
     # populate_books_and_tags()
-    populate_synonyms_from_tags()
+    # populate_synonyms_from_tags()
+    populate_lastfm_from_synonyms()
     
 
 if __name__ == '__main__':
