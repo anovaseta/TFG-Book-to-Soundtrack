73ae6fa4e2c135d6dcccb308f5d0311809a8b7b1
manuloseta <manuloseta@gmail.com>
2026-04-22 20:23:35 +0200

    production stuff 2

--- tfg_myproject/tfg_webpage/tfg_webpage/settings.py
+++ tfg_myproject/tfg_webpage/tfg_webpage/settings.py
@@ -11,21 +11,30 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fik(y#q7)kseaiqh+k8$g8w&itx@8p3v8)2abq8a8xn$r)y**='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-fik(y#q7)kseaiqh+k8$g8w&itx@8p3v8)2abq8a8xn$r)y**='
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
